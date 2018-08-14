cdd062eed66e9b734a49afde4fb65dffd683a30d
vikiwebdeveloper <vikiwebdeveloper@gmail.com>
2018-08-14 06:36:37 -0400

    Initializing Register and Index PHP

--- php/index.php
+++ php/index.php
@@ -1,3 +1,17 @@
 <?php
 echo "Welcome to the PHP Home"
 ?>
+<html>
+  <head>
+    <title>Login page</title>
+  </head>
+  <body>
+    <h2>
+      Login
+    </h2>
+    <label>Username</label>
+    <input type="text" name="username" />
+    <label>Password</label>
+    <input type="text" name="password" />
+  </body>
+</html>
