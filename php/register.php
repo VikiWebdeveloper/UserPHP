79943d0892f3949c9ab2f2f4ff298bb580c47d56
vikiwebdeveloper <vikiwebdeveloper@gmail.com>
2018-08-14 06:31:43 -0400

    Initializing Register and Index PHP

---
+++ php/register.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>Register yourself</title>
+   </head>
+  <div>
+    <h2>
+      Register
+    </h2>
+  </div>
+  <div class="form-group">
+    <label>Username</label><input type="username" />
+    <label>Email</label><input type="email" />
+    <label>Password</label><input type="password" />
+    <label>Confirm Password</label><input type="password1" />
+  </div>
+</html>
