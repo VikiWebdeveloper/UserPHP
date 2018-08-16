964514b917289348bd4247e3d3ebb711424c314f
vikiwebdeveloper <vikiwebdeveloper@gmail.com>
2018-08-16 06:05:06 -0400

    Completed Registration module

--- php/index.php
+++ php/index.php
@@ -1,4 +1,17 @@
 <?php
 include("server.php");
-echo $_SESSION['success'];
 ?>
+<html>
+<head>
+    <title>Login page</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </head>
+<body class="container">
+  <a href="login.php">Sign in</a> 
+  <a href="register.php">Sign up</a>  
+</body>
+
+</html>
